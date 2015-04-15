7721e2cc474bd9b5de7913d9dc4019686cbca7bb
Vit Hodes <xhodes00@stud.fit.vutbr.cz>
2015-04-15 17:54:59 +0200

    Trida filtru..

--- src/filter.cpp
+++ src/filter.cpp
@@ -1,5 +1,6 @@
 #include "filter.h"
 
+
 filter::filter()
 {
 
@@ -10,3 +11,38 @@ filter::~filter()
 
 }
 
+
+//% A - matrix with predictor coefficients (each vector in a column, no a0 coefficient)
+//takze predpokladam ze b0 je vzdy 1
+float filter::do_step(std::vector<float> &fir_part, std::vector<float> &iir_part, float signal){
+
+
+    //correcting the size of state and incoming coeficients to match
+    while(state.size() < fir_part.size() || state.size() < iir_part.size())
+        state.push_back(0.0f);
+
+    while(fir_part.size() < iir_part.size())
+        fir_part.push_back(0.0f);
+
+    while(iir_part.size() < fir_part.size())
+        iir_part.push_back(0.0f);
+
+
+    //bigger index -> bigger delay
+
+    float history = 0.0f;
+
+    for(int i = 0; i < state.size(); i++){
+
+        history += state[i] * fir_part[i] - state[i] * iir_part[i];
+
+    }
+
+
+    float result = signal + history;
+
+    state.push_front(signal);   //signal pro priste se spozdenim 1
+    state.pop_back();   //a posledni se zahodi..
+
+return result;
+}
--- src/filter.h
+++ src/filter.h
@@ -1,12 +1,20 @@
 #ifndef FILTER_H
 #define FILTER_H
 
-
+/*
+Filtr, stav si uchovava sam.
+Velikost jeho stavu zavisi na poctu vlozenych koeficientu, ktery by se ale nemel menit..
+*/
 class filter
 {
 public:
     filter();
+
     ~filter();
+
+    std::deque state;
+
+    float do_step(std::vector<float> &fir_part, std::vector<float> &iir_part, float signal);
 };
 
 #endif // FILTER_H
